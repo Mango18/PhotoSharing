1fa95f7dc7d1ad17bda3d5260306dcda06f1f84f
Jerss <nossrej18@hotmail.com>
2021-01-03 09:22:05 +0100

    entrada Identity

--- Identity/Global.asax.cs
+++ Identity/Global.asax.cs
@@ -1,23 +1,20 @@
-﻿using PhotoSharingApp.Data.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace PhotoSharingApp.Web
+namespace Identity
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
-
-          
-            // Database.SetInitializer<PhotoSharingContext>(new PhotoSharingInitializer());
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
